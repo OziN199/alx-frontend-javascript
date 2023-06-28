e8cf8039f76c3f9353144c955fd086330cbac03d
ozioma <nnekaozioma@gmail.com>
2023-06-28 06:02:59 +0000

    update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,13 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
- if (trueOrFalse) {
+
+  if (trueOrFalse) {
     const task = true;
     const task2 = false;
- if (task && task2) {
-    return [task, task2];
+    if (task && task2) {
+      return [task, task2];
+    }
   }
-}
+
   return [task, task2];
- }

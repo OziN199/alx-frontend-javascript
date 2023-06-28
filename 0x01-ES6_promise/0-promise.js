a4e1d1895e878066a17192785a88e7a18933b550
ozioma <nnekaozioma@gmail.com>
2023-06-28 13:56:54 +0000

    update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,3 @@
 function getResponseFromAPI()
 	return new promise((resolve, reject) => {
-     //simulating an asynchronous API call
-   setTimeout(() => {
-  //assuming API call is successfull
-
-  const response = "this is the API response";
-  resolve(response);
-  
-     // Assuming there was an error in the API call
-      // const error = "Error occurred during API call";
-      // reject(error);
-    }, 2000); // Simulating a 2-second delay
-  });
-}
+    }
